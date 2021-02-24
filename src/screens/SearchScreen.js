ec2b8a2e855f32dbe97a9fd80b4c982870d0ee08
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-24 16:25:44 -0500

    Detecting Editing Completion

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,7 +7,11 @@ const SearchScreen = () => {
     console.log(term)
     return(
         <View>
-            <SearchBar term={term} onTermChange={ (newTerm) => setTerm(newTerm)}/>
+            <SearchBar 
+                term={term} 
+                onTermChange={ (newTerm) => setTerm(newTerm)}
+                onTermSubmit={() => console.log('Submitted')}    
+            />
             <Text>Search Screen, {term}</Text>
             
         </View>
