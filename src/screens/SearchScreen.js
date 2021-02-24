74cf6f695b6935918664fedb3d5a5160b8521aff
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-24 14:58:55 -0500

    assembling the navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const SearchScreen = () => {
+    return(
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default SearchScreen;
+
