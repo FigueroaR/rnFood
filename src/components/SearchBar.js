963c376b442f1592640fe81943ae1fed7973bf94
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-24 15:35:32 -0500

    Atouch more of styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 import {Feather} from '@expo/vector-icons'
 
 const SearchBar = () => {
     return(
-        <View style={styles.background} >
+        <View style={styles.backgroundStyle} >
             <Feather name="search" size={30}/>
-            <Text>Search Bar</Text>
+            <TextInput
+                style={styles.inputStyle}
+                placeholder="Search"/>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    background: {
+    backgroundStyle: {
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 15,
-        marginHorizontal: 15
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        borderColor: 'black',
+        borderWidth: 1,
+        flex: 1 // use up as much space
     }
 })
 
