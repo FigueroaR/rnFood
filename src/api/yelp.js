ac56f4d6dd34a1df67ee3f5e3561c189a25a4a19
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-25 12:33:53 -0500

    back to useEffect and working/ no error

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -5,6 +5,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer cSwODGH1LwM0LPwFLEZIvDw2LK6O_E361Dy0p61WvH56Jj6ksMYoObI4XxOGqBzTCdJqyC_EWrqhC2E-30w7F4dwTDWa3AhpJmdgCeF5qFUJFid6azWWdvFLp6c2YHYx' 
+        Authorization: 'Bearer y2R8oGXlK6JN-NXdk2XXyGCtVxQUQnF6sxIikZ2V7sCoqRPYKmEQNNO0rW7whntrgY5K_w6AKfihfxXYEY-k2rNIZ-oY2_nXSvtgXRtRIvhFSXw8DWvnrGOGgdo3YHYx' 
     }
 })
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,25 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import SearchBar from '../components/SearchBar';
-import useResults from '../hooks/useResults';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import SearchBar from '../components/SearchBar'
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
-  const [term, setTerm] = useState('');
-  const [searchApi, results, errorMessage] = useResults();
+    const [term, setTerm] = useState('');
+    const [results, setResults] = useState([])
+    const [errorMessage, setErrorMessage] = useState('')
 
-  return (
-    <View>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={() => searchApi(term)}
-      />
-      {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-    </View>
-  );
-};
+    // we can make this async
+    // instead of using .then 
+    const searchApi = async () => {
 
-const styles = StyleSheet.create({});
+
+        // if anything goes wrong with our try block, we catch the error
+        try {
+            const response = await  yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose'
+                }
+            })
+            setResults(response.data.businesses)
+        } catch (err) {
+            setErrorMessage('Something went Wrong!')
+        }
+
+      
+    }
+    
+    useEffect(( )=> {
+        searchApi('Pasta')
+    }, [])
+
+    return(
+        <View>
+            {errorMessage ? <Text>{errorMessage}</Text> : null }
+            <SearchBar 
+                term={term} 
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}    
+            />
+            <Text>We have found {results.length} results </Text>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
 
 export default SearchScreen;
