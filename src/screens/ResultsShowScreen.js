cf2975b15019dd8aa81708da97f27a24bdfdd102
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-25 17:02:59 -0500

    Communicating between Screens

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native'
 
-const ResultsShowScreen = () => {
+const ResultsShowScreen = ({navigation}) => {
+    // we get navigation from onPress used on ResultsList
+    // inside navigation we have a function called getParam('')
+    const id = navigation.getParam('id')
+    
     return(
         <View>
             <Text>Results Show</Text>
