5c2fad70a79f9cf5785355a50361218338ac015e
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-25 14:00:36 -0500

    Grouping results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,6 +7,15 @@ import ResultsLists from '../components/ResultsList'
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
+  //console.log(results)
+
+  const filterResultsByPrice = (price) => {
+    return results.filter(result => {
+      //console.log(result.price)
+      return result.price === price
+    })
+  }
+
 
   return (
     <View>
@@ -17,9 +26,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsLists title="Cost Effective"/>
-      <ResultsLists title="Bit Pricier"/>
-      <ResultsLists title="Big Spender"/>
+      <ResultsLists results={filterResultsByPrice('$')} title="Cost Effective"/>
+      <ResultsLists results={filterResultsByPrice('$$')} title="Bit Pricier"/>
+      <ResultsLists results={filterResultsByPrice('$$$')} title="Big Spender"/>
     </View>
   );
 };
