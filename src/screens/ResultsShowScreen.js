05ad253b1dbd7988db4d2618d7bc77e0ca8f9cc6
FigueroaR <48839893+FigueroaR@users.noreply.github.com>
2021-02-25 17:15:27 -0500

    Fetching a single Restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native'
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({navigation}) => {
     // we get navigation from onPress used on ResultsList
     // inside navigation we have a function called getParam('')
     const id = navigation.getParam('id')
-    
+    const [result, setResult] = useState(null)
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data)
+    }
+    console.log(result)
+
+    useEffect(() => {
+        getResult(id)
+    }, [])
+
     return(
         <View>
-            <Text>Results Show</Text>
+            <Text>Results Showw</Text>
         </View>
     )
 }
